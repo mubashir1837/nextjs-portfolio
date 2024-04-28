c42f3dbeea74f7129a97283c3dec391177e9154a
My Name <someone@email.com>
2024-04-28 23:51:59 +0500

    portfolio

--- next.config.js
+++ next.config.js
@@ -2,9 +2,3 @@
 const nextConfig = {}
 
 module.exports = nextConfig
-
-function throwError(envVar) {
-    throw `Abort: You need to define ${envVar} in the .env file.`
-  }
-  
-  if (!process.env.RESEND_API_KEY) return throwError('RESEND_API_KEY');
