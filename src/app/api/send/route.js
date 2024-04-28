a7b8a349e2172b682dab2ffa62969b3f971a3711
My Name <someone@email.com>
2024-04-29 00:00:48 +0500

    portfolio

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("process.env.RESEND_API_KEY");
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
