80d874c5b1d95172f4512b75ee0df1c9bc9ac94d
My Name <someone@email.com>
2024-05-14 20:23:25 +0500

    portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -6,18 +6,9 @@ import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
 
-
-
 export default function Home() {
   return (
-    <main
-      className="flex min-h-screen flex-col"
-      style={{
-        backgroundImage: `url('/images/backgroun.jpg')`, // Relative path from the root
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-      }}
-    >
+    <main className="flex min-h-screen flex-col bg-gradient-to-r from-[#463f96] to-[#1db9b9]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
