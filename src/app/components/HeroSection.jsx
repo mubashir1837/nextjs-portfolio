11e7de84f3b1c15a973661fba99a8061cea299ac
My Name <someone@email.com>
2024-05-24 13:05:20 +0500

    portfolio

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -62,7 +62,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-gradient-to-r from-[#090d82] to-secondary-600 w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative">
+          <div className="rounded-full bg-gradient-to-r from-[#090d82] to-secondary-600  shadow-2xl shadow-[#090d82] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative">
             <Image
               src="/images/hero-image.jpg"
               alt="hero image"
